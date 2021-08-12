2148129eee3daf460cfb625be1bfc59e66958b3e
Mahesh-Shiva <mahesh@ridecell.com>
2021-08-12 14:49:39 +0530

    Removed email config from settings

--- btre/settings.py
+++ btre/settings.py
@@ -147,18 +147,10 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Messages
-
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
-
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'shiva.mahesh59@gmail.com'
-EMAIL_HOST_PASSWORD = 'BROKENARROW57'
-EMAIL_USE_TLS = True
 
 try:
     from .local_settings import *

27d1004ea998076b8f32334ed9b18c21d57af1ee
Mahesh-Shiva <mahesh@ridecell.com>
2021-08-12 14:42:43 +0530

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -159,3 +159,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'shiva.mahesh59@gmail.com'
 EMAIL_HOST_PASSWORD = 'BROKENARROW57'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
